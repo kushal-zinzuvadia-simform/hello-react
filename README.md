a66eeafa2b852b68af86c8a6519e577101b26ad6
Kushal Zinzuvadia <kushal.zinzuvadia@simformsolutions.com>
2026-05-25 16:38:13 +0530

    feat: Add scripts to run prettier, add commands in readme

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## Use Vite to create a React app
 
 ## Use ESLint and Prettier
+
+### Formatting
+
+- Run formatter (writes changes): `npm run format`
+- Check formatting (CI-friendly, exit non-zero on differences): `npm run format:check`
+
+Prettier is included as a devDependency in the project.
--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -4,7 +4,12 @@ function App() {
       <h1>Kushal Zinzuvadia</h1>
       <h2>Trainee</h2>
       <p>Email: kushal.zinzuvadia@simformsolutions.com</p>
-      <p>GitHub: <a href="https://github.com/kushal-zinzuvadia-simform" target='_blank'>github.com/kushal-zinzuvadia-simform</a></p>
+      <p>
+        GitHub:{' '}
+        <a href="https://github.com/kushal-zinzuvadia-simform" target="_blank">
+          github.com/kushal-zinzuvadia-simform
+        </a>
+      </p>
     </div>
   );
 }
